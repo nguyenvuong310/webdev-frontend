257753c289f735bc3ead2652dded793fbf0a2ddf
nguyenvuong310 <trungvuong2169@gmail.com>
2025-01-18 03:11:14 +0700

    test deploy

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -11,6 +11,7 @@ import {
 
 import TableScore from '../../components/Tables/TableScore';
 const ECommerce: React.FC = () => {
+  console.log(process.env.API_URL);
   return (
     <>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
